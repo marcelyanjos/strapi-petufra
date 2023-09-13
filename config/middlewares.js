257caa45116496454be2e11edf39686e49b0b1cb
Marcely Anjos <marcely254@gmail.com>
2023-09-12 23:10:02 -0300

    correção de bugs

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,20 @@
-module.exports = [
+module.exports = ({ env }) => [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', `https://${env('AWS_BUCKET')}.s3.amazonaws.com`],
+          'media-src': ["'self'", 'data:', 'blob:', `https://${env('AWS_BUCKET')}.s3.amazonaws.com`],
+          'frame-ancestors': ["'self'", 'https:', 'http:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
